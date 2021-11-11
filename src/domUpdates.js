7795e33472266024133c83a6fbd2165c37cbac08
Nate Ewert-Krocker <n.ewertkrocker@gmail.com>
2021-11-11 16:50:51 -0500

    Format header; new color scheme; format trip cards

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -2,20 +2,26 @@ import { currentTraveler } from './scripts.js';
 
 const updateDOM = (currentTraveler, trips, destinations) => {
   buildTravelCardGrid(currentTraveler, trips, destinations)
+  displayGreeting(currentTraveler);
 };
+
+const displayGreeting = (currentTraveler) => {
+  let greeting = `Welcome, ${currentTraveler.name}!`
+  travelerGreeting.innerText = greeting.toUpperCase();
+}
 
 const buildTravelCardGrid = (currentTraveler, trips, destinations) => {
   let userTrips = trips.getAllUserTrips(currentTraveler.id);
   userTrips.forEach((trip) => {
     const destination = destinations.getDataByID(trip.destinationID);
-    console.log(destination[0]);
     const lodgingCosts = destinations.getTotalLodgingCosts(destination[0].id, trip);
     const flightCosts = destinations.getTotalFlightCosts(destination[0].id, trip);
     const destinationImage = destination[0].image;
     tripGrid.innerHTML += `<article class="trip-card">
           <div class="destination-photo" style="background-image: url(${destinationImage});">
-              <p class="trip-dates">${trip.date} dates</p>
+              <p class="trip-shader">
               <h1 class="destination-name">${destination[0].destination}</h1>
+              <p class="trip-dates">${trip.date}</p>
           </div>
           <div class="trip-info">
             <h2>COSTS:</h2>
@@ -32,6 +38,7 @@ const buildTravelCardGrid = (currentTraveler, trips, destinations) => {
 // QUERY SELECTORS
 
 const tripGrid = document.getElementById('tripGrid');
+const travelerGreeting = document.getElementById('travelerGreeting');
 
 export default updateDOM;
 export {};
