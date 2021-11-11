989d90440c1ea617c0ea93bca6d0b128327900ef
Nate Ewert-Krocker <n.ewertkrocker@gmail.com>
2021-11-11 10:46:09 -0500

    Set up generated trip cards

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -6,12 +6,32 @@ const updateDOM = (currentTraveler, trips, destinations) => {
 
 const buildTravelCardGrid = (currentTraveler, trips, destinations) => {
   let userTrips = trips.getAllUserTrips(currentTraveler.id);
-
+  userTrips.forEach((trip) => {
+  tripGrid.innerHTML += `<article class="trip-card">
+          <div class="destination-photo">
+            <div>
+              <p class="trip-dates">${trip.date} dates</p>
+            </div>
+          </div>
+          <div class="trip-info">
+            <h1 class="destination-name">${trip.destinationID}Trip Destination</h1>
+            <blockquote>
+              *url blockquote here*
+            </blockquote>
+            <h2>COSTS:</h2>
+            <p class="lodging-costs">Lodging</p>
+            <p class="trip-costs">flight</p>
+            <h3>Suggested activities:</h3>
+            <p>${trip.suggestedActivities}activities here</p>
+            <!-- card body -->
+          </div>
+      </article>`
+  })
 }
 
 // QUERY SELECTORS
 
-
+const tripGrid = document.getElementById('tripGrid');
 
 export default updateDOM;
 export {};
