c6c62ac5d9353c56da04f9eee5e4009c5e7be3c9
Nate Ewert-Krocker <n.ewertkrocker@gmail.com>
2021-11-13 19:11:19 -0500

    Change CSS and generated card HTML to make more accessible with keyboard

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -27,7 +27,7 @@ const buildTravelCardGrid = (currentTraveler, trips, destinations) => {
     const flightCosts = destinations.getTotalFlightCosts(destination[0].id, trip);
     const destinationImage = destination[0].image;
     const tripEnd = dayjs(trip.date).add(trip.duration, 'day').format('YYYY/MM/DD');
-    tripGrid.innerHTML += `<article class="trip-card">
+    tripGrid.innerHTML += `<article class="trip-card" tabindex="0">
           <div class="destination-photo" style="background-image: url(${destinationImage});">
               <p class="trip-shader">
               <h1 class="destination-name">${destination[0].destination}</h1>
@@ -39,31 +39,51 @@ const buildTravelCardGrid = (currentTraveler, trips, destinations) => {
             <p class="lodging-costs">Lodging: $${lodgingCosts}</p>
             <p class="trip-costs">Flight: $${flightCosts}</p>
             <h3>Suggested activities:</h3>
-            <p>${trip.suggestedActivities}activities here</p>
+            <p>${trip.suggestedActivities}</p>
           </div>
       </article>`
   })
 }
 
 const addTripRequestCard = (currentTraveler, trips, destinations) => {
-  tripGrid.innerHTML += `<article class="trip-card plan-trip-card">
+  tripGrid.innerHTML += `<article class="trip-card" id="planTripCard" data-micromodal-trigger="modal-1" tabindex='0'>
       <div class="destination-photo" style="background-image: url('./images/earthfromspace.jpg');">
         <p class="trip-shader">
-        <h1 class="destination-name next-adventure">Your next adventure</h1>
+        <h1 class="destination-name next-adventure">Your next adventure!</h1>
       </div>
-      <div class="new-trip-button" id="newTripButton" data-micromodal-trigger="modal-1">
-        <img src="./images/old-compasses.svg" id="compassesIcon">
+      <div class="new-trip-button" id="newTripButton">
+        <img src="./images/old-compasses.svg" alt="Compass Icon -- Plan a new trip!" id="compassesIcon">
         <h2>PLAN A NEW TRIP</h2>
       </div>
     </article>`
     MicroModal.init();
 }
 
+const checkKey = (event) => {
+  if(event.key === 'Space'){
+    openModal(event);
+  } else if(event.key === 'Enter') {
+    openModal(event);
+  }
+};
+
+const openModal = (event) => {
+  if(event.target.id === 'planTripCard'){
+    MicroModal.show('modal-1');
+  }
+};
+
 // QUERY SELECTORS
 
 const tripGrid = document.getElementById('tripGrid');
 const travelerGreeting = document.getElementById('travelerGreeting');
 const totalTravelCosts = document.getElementById('totalTravelCosts');
+const planTripCard = document.getElementById('planTripCard');
+
+// EVENT LISTENERS
+
+tripGrid.addEventListener('keydown', checkKey);
+
 
 export default updateDOM;
 export {};
